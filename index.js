f5dcd3f17c946fe759b8c07155f4835db63858f6
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-06-01 22:36:39 -0400

    sexo

--- index.js
+++ index.js
@@ -20,17 +20,23 @@ const itensRoutes = require('./routes/itens');
 const addRouters = require('./routes/add');
 
 //routes
-
 app.get('/', async (req, res) => {
     try {
-        // Obter todos os itens
+        let searchResults;
+        // Verificar se o parâmetro de consulta 'brand' está presente
+        if (req.query.brand) {
+            // Se estiver, usar esse termo para pesquisar na coleção de itens
+            searchResults = await db.getDb().db().collection('itens').find({ brand: req.query.brand }).toArray();
+        }
+
+        // Buscar todos os itens
         const items = await db.getDb().db().collection('itens').find({}).toArray();
 
         // Contar o número total de itens
         const totalItems = await db.getDb().db().collection('itens').countDocuments();
         
         // Renderizar a view 'home' com ambos os dados
-        res.render('home', { items, totalItems });
+        res.render('home', { items, totalItems, searchResults });
     } catch (err) {
         console.error('Erro ao buscar itens e contagem de documentos:', err);
         res.status(500).send('Erro no servidor');
